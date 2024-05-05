583ff151c9aadcfe17ad67cc663c81a3f5af7a43
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-05-06 00:10:09 +0300

    start

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ py -m venv venv - создание локального окружения
 pip freeze > requirements.txt - соххранение локальных зависимостей
 
 uvicorn main:app --reload - запуск сервера
+http://127.0.0.1:8000/docs#/ - Swagger или localhost:8000
 py pip install git+https://github.com/fiztexlabs/librsp.git - установка библиотеки
--- calculations/router.py
+++ calculations/router.py
@@ -1,6 +1,7 @@
+from enum import Enum
 from fastapi import APIRouter
-
-from shemas.model import CoilHe
+from pydantic import BaseModel, ValidationError
+import rsp
 
 router = APIRouter(
     prefix="/calculations",
@@ -8,8 +9,28 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def calculation(coilHe: CoilHe):
+# @router.get("/")
+# def calculation():
+#    h2o = rsp.createSubstance("H2O_IF97")
+#    # h2o =
+#    data = rsp.callProperty(h2o, "D", "PT", [101325, 300])
+#    return {"data": data}
 
-    res = coilHe
-    return {"data": res.geometry}
+
+@router.get("/")
+def init():
+    try:
+        h2o = rsp.createSubstance("H2O_IF96")
+        h2o_info = rsp.info.SubstanceInfo()
+        rsp.info.getSubstanceInfo(h2o, h2o_info)
+        modes = rsp.VectorString()
+        h2o_info.getCalcModesInfo(modes)
+        return {"data": {"h20": {"calc_modes": list(modes)}}, "status": "200"}
+    except RuntimeError as e:
+        print(e)
+        return {"data": {}, "status": "201"}
+    # h2o = rsp.createSubstance("H2O_IF92")
+    # for mode in modes:
+    #     result = []
+    #     result.append(mode)
+    #     return result
