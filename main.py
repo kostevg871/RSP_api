cfdfa9aca51c28726224f09937898cfe8efa017f
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-05-07 21:17:38 +0300

    refactor src, add getSubstanceList, getSubstanceParametersMode

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
-from calculations.router import router as router_calculation
+from init import substaneces_objects_globals
+from schemas import AvailableSubstance, ParameterMode
 
 
 app = FastAPI(
@@ -10,9 +11,11 @@ app = FastAPI(
 )
 
 
-@app.get("/getSubstancesList")
-def start():
-    return {"info": "Сервер работает!"}
+@app.get("/getSubstancesList", response_model=AvailableSubstance, description="Получение всех доступных веществ")
+def get_available_substances():
+    return {"data": substaneces_objects_globals.data_get_substances_list}
 
 
-app.include_router(router_calculation)
+@app.get("/getSubstanceParametersMode", response_model=ParameterMode, description="Запрос для получения Режима параметров")
+def get_calc_model_substanse(id):
+    return {"data": substaneces_objects_globals.data_get_calc_modes_info[int(id)]}
---
+++ schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+# Схемы ответов
+# схема вещества
+
+
+class Substance(BaseModel):
+    value: str
+    label: str
+
+# Схема ответа для вещества
+
+
+class AvailableSubstance(BaseModel):
+    data: list[Substance]
+
+# Схема расчета
+
+
+class Model(Substance):
+    value: str
+    label: str
+
+
+class ParameterMode(AvailableSubstance):
+    data: list[Substance]
