4cac1cc5b3c8ce5d9ca850c2529232151df6a98f
Konovalov <fizteh.science@yandex.ru>
2024-05-10 02:30:42 +0300

    Add getPropertiesTable request

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from typing import Union
 from fastapi import FastAPI
 
 from init import InitRSP
-from schemas import AvailableSubstance, ParameterMode, Property
+from schemas import *
+
+import rsp
 
 
 app = FastAPI(
@@ -25,3 +27,19 @@ def get_calc_model_substanse(id):
 @app.get("/getPropertiesLists", response_model=Property, description="Запрос для получения возможных Параметров для выбранного вещества и режима")
 def get_calc_model_substanse(substanceId, modeId):
     return {"data": list(app.substaneces_objects_globals.available_properties[int(substanceId)][str(modeId)])}
+
+# надо аккуратно сделать PropertyTableResponse
+@app.post("/getPropertiesTable", description="Запрос для получения Таблицы")
+def get_properties_table(request: PropertyTableRequest):
+    val = rsp.callProperty(
+        app.substaneces_objects_globals.substances_objects[int(request.substanceId)], 
+        request.params.propertyId, 
+        request.modeId,
+        request.params.value)
+
+    # response = PropertyTableResponse()
+    # response.dimensionId = "SI" # надо продумать
+    # response.propertyId = str(request.params.propertyId)
+    # response.value = str(val)
+
+    return {"data": {"dimensionId": "SI", "propertyId": str(request.params.propertyId), "value": str(val)}}
--- schemas.py
+++ schemas.py
@@ -21,10 +21,31 @@ class Model(Substance):
     value: str
     label: str
 
-
+# запрос 3
 class ParameterMode(AvailableSubstance):
     data: list[Substance]
 
 
 class Property(BaseModel):
     data: list[str]
+
+
+# ~~~ жесть для запроса 4
+class Params(BaseModel):
+    propertyId: str
+    value: list[float]
+    dimensionId: str
+
+class PropertyTableRequest(BaseModel):
+
+    substanceId: str # id выбранного вещества
+    modeId: str # id выбранного режима
+    params: Params
+
+class PropertyTableResponse(BaseModel):
+    propertyId: str
+    value: str
+    dimensionId: str
+
+# class PropertyTable(BaseModel):
+#         data: PropertyTableResponse
