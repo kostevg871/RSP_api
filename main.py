7d5c72833fd7f80feac91a75d9bb0051cc6dc878
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-05-11 23:51:44 +0300

    commit deploy

--- main.py
+++ main.py
@@ -19,22 +19,29 @@ app.substaneces_objects_globals = InitRSP()
 def get_available_substances():
     return {"data": app.substaneces_objects_globals.data_get_substances_list}
 
-# response_model=ParameterMode, 
+# response_model=ParameterMode,
+
+
 @app.get("/getCalcModesInfo", description="Запрос для получения Режима параметров")
 def get_calc_model_substanse(id):
     return {"data": app.substaneces_objects_globals.data_get_calc_modes_info[int(id)]}
 
-# response_model=Property, 
+# response_model=Property,
+
+
 @app.get("/getPropertiesLists", description="Запрос для получения возможных Параметров для выбранного вещества и режима")
 def get_calc_model_substanse(substanceId, modeId):
     return {"data": app.substaneces_objects_globals.properties[int(substanceId)][str(modeId)]}
 
 # response_model=PropertyTableResponse
+
+
 @app.post("/getPropertiesTable", description="Запрос для получения Таблицы")
 def get_properties_table(request: PropertyTableRequest):
     val = rsp.callProperty(
-        app.substaneces_objects_globals.substances_objects[int(request.substanceId)], 
-        request.params.propertyId, 
+        app.substaneces_objects_globals.substances_objects[int(
+            request.substanceId)],
+        request.params.propertyId,
         request.modeId,
         request.params.value)
 
@@ -44,10 +51,10 @@ def get_properties_table(request: PropertyTableRequest):
     # response.value = str(val)
 
     return {
-        "data": 
+        "data":
         {
-            "dimensionId": "SI", 
-            "property": str(app.substaneces_objects_globals.properties[int(request.substanceId)][str(request.modeId)][request.params.propertyId]), 
+            "dimensionId": "SI",
+            "property": str(app.substaneces_objects_globals.properties[int(request.substanceId)][str(request.modeId)][request.params.propertyId]),
             "value": str(val)
         }
     }
