a52fc85c769ae7724608b9a35566793f62adead5
Evgeniy <kostyrkoevgeniy@yandex.com>
2024-06-13 23:50:43 +0300

    fix outputs negative

--- core/convert_params_in_si.py
+++ core/convert_params_in_si.py
@@ -1,3 +1,4 @@
+from math import copysign
 from unit_converter.converter import convert, converts
 
 from core.init import InitRSP, rsp_callProperty
@@ -12,17 +13,16 @@ def convert_params_in_SI(substaneces_objects_globals: InitRSP, substanceId: int,
 
     for index, dimens in enumerate(params.param_dimensions):
         if dimens in PROPERTY_MIN_DIM.keys():
-            if params.param_values[index] < 0:
+            if params.param_values[index] <= 0:
                 value = params.param_values[index]
 
                 if params.param_dimensions[index] == "°C":
-                    print(1)
                     params.param_values[index] = value + 273.15
                     params.param_dimensions[index] = "K"
                 if params.param_dimensions[index] == "°F":
-                    params.param_values[index] = (value + 459.67) * 5 / 9
+                    params.param_values[index] = value + 459.67
                     params.param_dimensions[index] = "K"
-    print(params)
+
     params_in_SI = [
         float(convert(str(v) + ' ' + d, PROPERTY_DIMENSION_SI[l])) for v, d, l in zip(
             params.param_values,
@@ -36,7 +36,9 @@ def convert_params_in_SI(substaneces_objects_globals: InitRSP, substanceId: int,
         mode,
         params_in_SI)
 
-    val_dim = float(converts(str(
-        val) + ' ' + PROPERTY_DIMENSION_SI[property], params.property_dimension))
+    # Если будет приходить отрицательные значения температур и тп
+    # надо будет проверять
+    val_dim = copysign(float(converts(str(
+        abs(val)) + ' ' + PROPERTY_DIMENSION_SI[property], params.property_dimension)), val)
 
     return val_dim
