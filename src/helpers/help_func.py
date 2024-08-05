04cceb0cadef5729a315326f5a66a9a98979df0b
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-08-05 22:40:14 +0300

    refactor

--- src/helpers/help_func.py
+++ src/helpers/help_func.py
@@ -2,7 +2,7 @@ import decimal
 ctx = decimal.Context()
 
 
-def float_to_str(f, prec=18):  # 18 знаков хватит всем (c) BG
+def float_to_str(f, prec=18):
     ctx.prec = prec
     d1 = ctx.create_decimal(repr(f))
     return format(d1, 'f')
