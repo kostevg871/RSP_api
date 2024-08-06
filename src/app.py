c52e6ae9c46b66deb912a64ba30960cf6ca9135e
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-08-06 23:40:21 +0300

    init_py app run

--- src/app.py
+++ src/app.py
@@ -46,5 +46,5 @@ app.add_middleware(CORSMiddleware, allow_origins=origins,
 app.include_router(router=router_substances)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1")
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="127.0.0.1")
