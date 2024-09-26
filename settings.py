6e5ebb4ff8feabc6137dcbc248dbbf806ac23577
Evgeniy <kostyrkoevgeniy@yandex.com>
2024-09-26 20:52:43 +0300

    test_1

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ env = Env()
 
 REAL_DATA_BASE = env.str(
     "REAL_DATA_BASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@localhost:5433/postgres"
+    default="postgresql+asyncpg://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 )
