39aa102e84aa956b67cfd96dfcff74651d36a591
Evgeniy <kostyrkoevgeniy@yandex.com>
2024-09-27 15:38:53 +0300

    db real base env

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
+DB_HOST_REAL_DB = os.environ.get("DB_HOST_REAL_DB")
 VERIFICATION_TOKEN_SECRET = os.environ.get(
     "APP_CONFIG__ACCESS_TOKEN__VERIFICATION_TOKEN_SECRET")
 RESET_PASSWORD_TOKEN_SECRET = os.environ.get(
