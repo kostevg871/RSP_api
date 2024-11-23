3052a0c0ed294b57c83da9cff0b4290a15830076
Evgeniy <kostyrkoevgeniy@yandex.com>
2024-11-23 23:50:59 +0300

    fix env

--- settings.py
+++ settings.py
@@ -10,12 +10,12 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("POSTGRES_DB")
 DB_USER = os.environ.get("POSTGRES_USER")
 DB_PASS = os.environ.get("POSTGRES_PASSWORD")
-DB_HOST_REAL_DB = os.environ.get("DB_HOST_REAL_DB")
+DB_HOST = os.environ.get("DB_HOST")
 
 REAL_DATA_BASE = env.str(
     "REAL_DATA_BASE_URL",
     default=f"postgresql+asyncpg://{DB_USER}:{
-        DB_PASS}@{DB_HOST_REAL_DB}:{DB_PORT}/{DB_NAME}"
+        DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
 
